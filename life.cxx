7357c54c56f3e2eeb2f9f8024f01df939858d9a0
joyhz <jhzhu@scu.edu>
2025-10-29 20:52:02 -0700

    updated indentation

--- life.cxx
+++ life.cxx
@@ -11,7 +11,14 @@
 namespace csen79 {
 
 // student to rewrite both
-Life Life::cross(const Life& other) const { Life child; return child; }
-const unsigned int Life::dnaMatch(int c, const DNA& other) const {return 0;}
+Life Life::cross(const Life& other) const {
+    Life child;
+    
+    return child;
+}
+const unsigned int Life::dnaMatch(int c, const DNA& other) const {
+    
+    return 0;
+}
 
 }   // namespace csen79
