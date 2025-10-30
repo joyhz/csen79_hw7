34cdcdce3a0dc33d80b175b391705fd8b8513366
rvattuone-cyber <rvattuone@scu.edu>
2025-10-30 15:34:13 -0700

    Update life.cxx

--- life.cxx
+++ life.cxx
@@ -13,7 +13,15 @@ namespace csen79 {
 // student to rewrite both
 Life Life::cross(const Life& other) const {
     Life child;
-    
+    for(int c = 0; c < NChromo; c++) {
+        for(size_t b = 0; b < dna[c].size(); b++) {
+            bool use_this = (rand_int(0, 1) == 1);
+            bool bit = use_this ? dna[c].getCode(static_cast<int>(b)) : other.dna.getCode(static_cast<int>(b));
+            child.dna[c].setCode(static_cast<int>(b), bit);
+        }
+    }
+    child.newLife();
+    child.generation = std::max(this->generation, other.generation) + 1;
     return child;
 }
 const unsigned int Life::dnaMatch(int c, const DNA& other) const {
