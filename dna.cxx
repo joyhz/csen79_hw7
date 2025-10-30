4b31c45fd4652c2178c901de5b4a9be61c339786
joyhz <jhzhu@scu.edu>
2025-10-30 00:53:18 -0700

    dna.cxx overall done (just need to run it and check + not sure abt unsigned int)

--- dna.cxx
+++ dna.cxx
@@ -14,26 +14,61 @@ DNA::DNA(unsigned int v) {
 }
 
 bool DNA::getCode(const int i) const {
-    if (i < 0 || i >= nCode) return false;
-    return (static_cast<unsigned int>(codes) >> i) & 1U;
+    if (i < 0 || i >= nCode) return false;    // Checking if the bit position is within the valid range
+
+    /* Shifting right and masking with 1U to isolate only the last bit
+     * bit i = 1 --> result is 1, true
+     * bit i = 0 --< result is 0, false
+    */
+    return (static_cast<unsigned int>(codes) >> i) & 1U;    // the unsigned short might work here too
 }
 
 void DNA::setCode(const int i, bool v) {
+    if (i < 0 || i >= nCode) return;    // Checking if the bit position is within the valid range
 
+    if (v) {    // if v is true
+        // Creating a mask with 1 in the ith position + ORing it with codes to only set bit i to 1
+        codes |= (1U << i);
+    }
+    else {    // if v is false
+        // Creating a mask with 1 in the ith position + ANDing the inverted mask with codes to only set that bit to 0
+        codes &= ~(1U << i);
+    }
+    return;
 }
 
 size_t DNA::size() const {
-
-    return 0;
+    return nCode;
 }
 
 size_t DNA::count() const {
-
-    return 0;
+    size_t cnt = 0;
+    unsigned int val = 0;
+    val = codes;
+    while (val != 0) {    // Traversing while val is valid
+        if (val & 1U) {   // Checking if the lowest bit is 1
+            ++cnt;
+        }
+        val >>= 1;        // Shifting val right by 1 to move on to the next bit
+    }
+    return cnt;
 }
 unsigned int DNA::matchDNA(const DNA& other) const {
-    
-    return 0;
+    unsigned int cnt = 0;
+    unsigned int sameBits = 0;
+    /* codes ^ other.codes: XORing the bits for codes and other.codes, giving 1 if the bits are different and 0 if they match
+     * ~(codes ^ other.codes): Now we'll get 1 where the bits match and 0 where they're different
+     * Using bitwise AND to ensure we only look at the lower 16 bits since DNA only uses 16 codes (remember nCode is 16)
+    */
+    sameBits = ~(codes ^ other.codes) & mask;
+
+    while (sameBits != 0) {
+        if (sameBits & 1U) {    // Checking if the LSB is 1
+            ++cnt;
+        }
+        sameBits >>= 1;    // Shifting sameBits right by 1 to test the next bit
+    }
+    return cnt;
 }
 
 }   // namespace csen79
